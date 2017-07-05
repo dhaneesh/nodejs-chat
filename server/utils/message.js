5b75807d97bdb1311b2ff5d3a26c5742465123ad
Devasia, Dhaneesh <Dhaneesh.Devasia@nyumc.org>
2017-07-05 20:43:31 +0530

    first commit

---
+++ server/utils/message.js
@@ -0,0 +1,16 @@
+var moment = require('moment');
+var generateMessage = (from, text) => {
+  return {
+    from: from,
+    text: text,
+    createdAt: moment().valueOf()
+  }
+}
+var generateLocationMessage = (from, lat, long) => {
+  return {
+    from : from,
+    url : `https://www.google.com/maps?q=${lat},${long}`,
+    createdAt: moment().valueOf()
+  }
+}
+module.exports = {generateMessage, generateLocationMessage};
