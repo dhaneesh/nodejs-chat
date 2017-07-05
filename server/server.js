2d59bb0d27d571f7fdf6f0f4c4ff9b6dfe064d08
Devasia, Dhaneesh <Dhaneesh.Devasia@nyumc.org>
2017-07-05 21:05:59 +0530

    removed unwanted files

--- server/server.js
+++ server/server.js
@@ -13,7 +13,6 @@ app.use(express.static(publicPath));
 
 io.on('connection', function(socket) {
   console.log('New user connected');
-
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
